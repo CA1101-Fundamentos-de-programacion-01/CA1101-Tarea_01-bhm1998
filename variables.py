ee42a9502427358f8601f5a879baf2b7ac9e14c4
bhm1998 <98926989+bhm1998@users.noreply.github.com>
2022-02-05 11:37:15 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,8 +1,9 @@
-z = 20
+monedas_encontradas=20
 
-x = 10
+monedas_magicas=10
 
-y = 3
+monedas_robadas=3
 
 
-resultado = z + x * 365 - y * 52
+(monedas_encontradas+monedas_magicas*365)-(monedas_robadas*52)
+
